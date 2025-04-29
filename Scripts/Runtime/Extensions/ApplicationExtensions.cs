09832c11c5ec00fe6c4882e5dd63072412d42140
GUnity <guillem.unity@gmail.com>
2025-04-29 16:25:49 +0200

    Addition

---
+++ Scripts/Runtime/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,14 @@
+namespace GUtilsUnity.Extensions
+{
+    public static class ApplicationExtensions
+    {
+        public static void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
